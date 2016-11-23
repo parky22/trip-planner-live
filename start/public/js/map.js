e363de7b94f0232608f23910cdcbea35a7719c7b
Joey Darbyshire <jmikeydarby1@gmail.com>
2016-11-23 11:46:33 -0500

    add and remove buttons done

--- start/public/js/map.js
+++ start/public/js/map.js
@@ -31,33 +31,38 @@ $(function initializeMap (){
     stylers: [{ color: '#b6c54c' }, { lightness: 40 }, { saturation: -40 }]
   }];
 
-  var mapCanvas = document.getElementById('map-canvas');
+  mapCanvas = document.getElementById('map-canvas');
 
-  var currentMap = new google.maps.Map(mapCanvas, {
+  currentMap = new google.maps.Map(mapCanvas, {
     center: fullstackAcademy,
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: styleArr
   });
 
-  var iconURLs = {
+  iconURLs = {
     hotel: '/images/lodging_0star.png',
     restaurant: '/images/restaurant.png',
     activity: '/images/star-3.png'
   };
-
-  function drawMarker (type, coords) {
-    var latLng = new google.maps.LatLng(coords[0], coords[1]);
-    var iconURL = iconURLs[type];
-    var marker = new google.maps.Marker({
-      icon: iconURL,
-      position: latLng
-    });
-    marker.setMap(currentMap);
-  }
 
   // drawMarker('hotel', [40.705137, -74.007624]);
   // drawMarker('restaurant', [40.705137, -74.013940]);
   // drawMarker('activity', [40.716291, -73.995315]);
 
 });
+
+var mapCanvas;
+var currentMap;
+var iconURLs;
+
+function drawMarker (type, coords) {
+  var latLng = new google.maps.LatLng(coords[0], coords[1]);
+  var iconURL = iconURLs[type];
+  var marker = new google.maps.Marker({
+    icon: iconURL,
+    position: latLng
+  });
+  marker.setMap(currentMap);
+  return marker;
+}
